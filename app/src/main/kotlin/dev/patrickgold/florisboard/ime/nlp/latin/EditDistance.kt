223f6b82681e008b4cd14f20e7db5caf72ac50eb
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-10-09 20:22:53 -0400

    in-progress work

--- app/src/main/kotlin/dev/patrickgold/florisboard/ime/nlp/latin/EditDistance.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/ime/nlp/latin/EditDistance.kt
@@ -36,41 +36,100 @@ data class VertexState<T>(val state: State, val key: T?) {
     }
 }
 
+data class Distance(val dist: Int)
+
 data class Vertex<T>(val key: VertexState<T>)
 
-data class DirectedEdge<T>(val target: Vertex<T>)
+data class PathId(val id: Int)
+
+data class DirectedEdge<T>(val target: Vertex<T>, val pathId: PathId)
 
 // NB: finding all cycles of length less than some max including the query path (string)!
 class WordGraph(wordData: Map<String, Int>) {
-    private val edgeList: MutableMap<Vertex<Char>, MutableList<DirectedEdge<Char>>> = mutableMapOf();
+    private val edgeList: MutableMap<Vertex<Char>, MutableList<DirectedEdge<Char>>> = mutableMapOf()
+    private val pathLookup: MutableMap<PathId, List<Vertex<Char>>> = mutableMapOf()
+    private var pathIdCounter: Int = 0
+
+    private fun constructPath(word: String): Pair<PathId, List<Vertex<Char>>> {
+        assert(word.isNotBlank())
+        val path: MutableList<Vertex<Char>> = mutableListOf()
+        val curPath = PathId(pathIdCounter++)
+        var source: Vertex<Char> = Vertex(VertexState(State.Start, null))
+        for (ch in word) {
+            path.add(source)
+            edgeList[source] = edgeList.getOrElse(source) { -> mutableListOf() }
+            val innerTarget = Vertex(VertexState(State.Inner, ch))
+            edgeList[source]!!.add(DirectedEdge(innerTarget, curPath))
+            source = innerTarget
+        }
+        val finalTarget: Vertex<Char> = Vertex(VertexState(State.End, null))
+        path.add(finalTarget)
+        edgeList[source] = edgeList.getOrElse(source) { -> mutableListOf() }
+        edgeList[source]!!.add(DirectedEdge(finalTarget, curPath))
+
+        pathLookup[curPath] = path
+
+        return Pair(curPath, path)
+    }
 
     // Populate graph.
     init {
         // TODO: integrate word frequency!
         for ((word, _freq) in wordData) {
-            assert(word.isNotBlank())
-            var source: Vertex<Char> = Vertex(VertexState(State.Start, null))
-            for (ch in word) {
-                edgeList[source] = edgeList.getOrElse(source) { -> mutableListOf() }
-                val innerTarget = Vertex(VertexState(State.Inner, ch));
-                edgeList[source]?.add(DirectedEdge(innerTarget))
-                source = innerTarget
-            }
-            val finalTarget: Vertex<Char> = Vertex(VertexState(State.End, null))
-            edgeList[source] = edgeList.getOrElse(source) { -> mutableListOf() }
-            edgeList[source]?.add(DirectedEdge(finalTarget))
+            constructPath(word);
         }
     }
 
     fun querySimilar(maxDepth: Int, query: String): List<ConsultResult> {
-        val path: MutableList<Vertex<Char>> = mutableListOf(Vertex(VertexState(State.Start, null)))
-        for (ch in query) {
-            path.add(Vertex(VertexState(State.Inner, ch)))
-        }
-        path.add(Vertex(VertexState(State.End, null)))
+        val (thisPath, path) = constructPath(query)
 
+        // Find all start and end vertices within maxDepth distance!
         val seen: MutableSet<Vertex<Char>> = mutableSetOf()
-        // TODO: find all start and end vertices within maxDepth distance
+        val pathsForVertex: MutableMap<Vertex<Char>, MutableList<Pair<PathId, Distance>>> =
+            mutableMapOf()
+        var queue: List<Vertex<Char>> = path.toList()
+        for (curDepth in 0..maxDepth) {
+            val curDistance = Distance(curDepth)
+            for (v in queue) {
+                seen.add(v)
+            }
+            val newQueue: MutableList<Vertex<Char>> = mutableListOf()
+            for (v in queue) {
+                edgeList[v] = edgeList.getOrElse(v) { -> mutableListOf() }
+                val edges = edgeList[v]!!
+                for ((target, curPath) in edges) {
+                    pathsForVertex[target] = pathsForVertex.getOrElse(target) { -> mutableListOf() }
+                    pathsForVertex[target]!!.add(Pair(curPath, curDistance))
+                    newQueue.add(target)
+                }
+            }
+            queue = newQueue.toList()
+        }
+
+        val startPathsFound: Map<PathId, Distance> =
+            pathsForVertex[Vertex(VertexState(State.Start, null))]!!.toMap()
+        val endPathsFound: Map<PathId, Distance> =
+            pathsForVertex[Vertex(VertexState(State.End, null))]!!.toMap()
+        val fullyFoundPaths: Set<PathId> = startPathsFound.keys.intersect(endPathsFound.keys)
+        val ret: MutableList<ConsultResult> = mutableListOf()
+        for (pathId in fullyFoundPaths) {
+            if (pathId == thisPath) continue
+
+            val startDist: Int = startPathsFound[pathId]!!.dist
+            val endDist: Int = endPathsFound[pathId]!!.dist
+            val trueDist = maxOf(startDist, endDist)
+            assert(trueDist > 0)
+            val confidence = (maxDepth - trueDist).toDouble() / maxDepth.toDouble()
+            var wordFromPath: String = ""
+            for (v in pathLookup[pathId]!!) {
+                if (v.key.state == State.Start || v.key.state == State.End) continue
+                assert(v.key.state == State.Inner)
+                wordFromPath += v.key.key!!
+            }
+            ret.add(ConsultResult(wordFromPath, confidence))
+        }
+
+        return ret
     }
 }
 
